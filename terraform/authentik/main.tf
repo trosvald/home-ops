8a530784c190d6eead9228e30cd07dddd9c92876
trosvald <eko.pur25@gmail.com>
2024-09-18 14:33:29 +0700

    feat(k8s): add ocis apps

--- terraform/authentik/main.tf
+++ terraform/authentik/main.tf
@@ -23,46 +23,10 @@ module "secret_authentik" {
   item   = "authentik"
 }
 
-module "secret_immich" {
-  source = "github.com/bjw-s/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "immich"
-}
-
 module "secret_grafana" {
   source = "github.com/bjw-s/terraform-1password-item?ref=main"
   vault  = "Automation"
   item   = "grafana"
-}
-
-module "secret_proxmox" {
-  source = "github.com/bjw-s/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "pve"
-}
-
-module "secret_nextcloud" {
-  source = "github.com/bjw-s/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "nextcloud"
-}
-
-module "secret_tandoor" {
-  source = "github.com/bjw-s/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "tandoor"
-}
-
-module "secret_audiobookshelf" {
-  source = "github.com/bjw-s/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "audiobookshelf"
-}
-
-module "secret_paperless" {
-  source = "github.com/bjw-s/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "paperless"
 }
 
 module "secret_ocis" {
@@ -72,6 +36,6 @@ module "secret_ocis" {
 }
 
 provider "authentik" {
-  url   = module.secret_authentik.fields["ENDPOINT_URL"]
-  token = module.secret_authentik.fields["TERRAFORM_TOKEN"]
+  url   = module.secret_authentik.fields["endpoint_url"]
+  token = module.secret_authentik.fields["api_token"]
 }
