4e4a8cafe063c3ee71a3ec51742f5aac22ab26eb
trosvald <eko.pur25@gmail.com>
2024-12-13 09:30:49 +0700

    chore: cleanup [skip ci]

--- tools/main.sh
+++ tools/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find kubernetes/main/talos/assets -type f | xargs -I{} sh -c "sed -i 's/$KUBERNETES_VERSION/{{ ENV.KUBERNETES_VERSION }}/g' {}"
-find kubernetes/main/talos/assets -type f | xargs -I{} sh -c "sed -i 's/$TALOS_VERSION/{{ ENV.TALOS_VERSION }}/g' {}"
+find kubernetes/prod/talos/assets -type f | xargs -I{} sh -c "sed -i 's/$KUBERNETES_VERSION/{{ ENV.KUBERNETES_VERSION }}/g' {}"
+find kubernetes/prod/talos/assets -type f | xargs -I{} sh -c "sed -i 's/$TALOS_VERSION/{{ ENV.TALOS_VERSION }}/g' {}"
