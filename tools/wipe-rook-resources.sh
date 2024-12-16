e7a59566c3f6af1c2422bd8d15b2917ffbf4e678
trosvald <eko.pur25@gmail.com>
2024-12-16 15:52:22 +0700

    chore: cleanup prod cluster [skip ci]

--- tools/wipe-rook-resources.sh
+++ tools/wipe-rook-resources.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # remove all rook resources
-kubectl patch cephfilesystems ceph-filesystem -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
-kubectl delete cephfilesystems ceph-filesystem -n rook-ceph
-kubectl patch cephblockpools ceph-blockpool -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
-kubectl delete cephblockpools ceph-blockpool -n rook-ceph
-kubectl patch cephobjectstores ceph-objectstore -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
-kubectl delete cephobjectstores ceph-objectstore -n rook-ceph
-kubectl patch CephFilesystemSubVolumeGroup ceph-filesystem-csi -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
-kubectl delete CephFilesystemSubVolumeGroup ceph-filesystem-csi -n rook-ceph
+kubectl --context prod patch cephfilesystems ceph-filesystem -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
+kubectl --context prod delete cephfilesystems ceph-filesystem -n rook-ceph
+kubectl --context prod patch cephblockpools ceph-blockpool -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
+kubectl --context prod delete cephblockpools ceph-blockpool -n rook-ceph
+kubectl --context prod patch cephobjectstores ceph-objectstore -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
+kubectl --context prod delete cephobjectstores ceph-objectstore -n rook-ceph
+kubectl --context prod patch CephFilesystemSubVolumeGroup ceph-filesystem-csi -p '{"metadata":{"finalizers":null}}' --type=merge -n rook-ceph
+kubectl --context prod delete CephFilesystemSubVolumeGroup ceph-filesystem-csi -n rook-ceph
