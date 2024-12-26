52cc547b921b3568df3230fb7e774fff5a7d2835
trosvald <eko.pur25@gmail.com>
2024-12-26 11:21:01 +0700

    fix(terraform): set region

--- terraform/minio/main.tf
+++ terraform/minio/main.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket                      = "terraform"
     key                         = "minio/state.tfstate"
+    region                      = "p-rh8-zfs"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
