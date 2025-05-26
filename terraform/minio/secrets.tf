b875d79b88ea39dfcfc6b8666354775bb6058d45
trosvald <eko.pur25@gmail.com>
2025-05-26 20:49:37 +0700

    ci: add terrraform gh wf

---
+++ terraform/minio/secrets.tf
@@ -0,0 +1,8 @@
+module "secrets" {
+  for_each          = toset(local.buckets)
+  source            = "./modules/create-secret"
+  name              = "${each.key}-bucket"
+  username          = random_password.user_name[each.key].result
+  password          = random_password.user_secret[each.key].result
+  onepassword_vault = data.onepassword_vault.kubernetes.uuid
+}
